ff16c675b3aa0b4ac76c50ca5293f69aa1dbf594
Erlite <ys.aameziane@gmail.com>
2021-03-05 00:30:12 +0100

    Add MoveFolder build stage

---
+++ UnrealBuildTool.Build/Stages/MoveFolder.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnrealBuildTool.Build.Stages
+{
+    public class MoveFolder : BuildStage
+    {
+        public override string GetName() => "MoveFolder";
+
+        public override string GetDescription()
+        {
+            TryGetConfigValue<string>("Target", out var target);
+            TryGetConfigValue<string>("Destination", out var destination);
+
+            target = target.Replace(@"\", "/").Replace("//", "/");
+            destination = destination.Replace(@"\", "/").Replace("//", "/");
+
+            if (target.Contains("/"))
+            {
+                target = target.Split('/').Last();
+            }
+            
+            if (destination.Contains("/"))
+            {
+                destination = target.Split('/').Last();
+            }
+
+            return $"Move folder '{target}' to '{destination}'";
+        }
+
+        public override void GenerateDefaultStageConfiguration()
+        {
+            base.GenerateDefaultStageConfiguration();
+            
+            AddDefaultConfigurationKey("Target", typeof(string), "");
+            AddDefaultConfigurationKey("Destination", typeof(string), "");
+        }
+
+        public override Task<StageResult> DoTaskAsync()
+        {
+            TryGetConfigValue<string>("Target", out var target);
+            TryGetConfigValue<string>("Destination", out var destination);
+
+            if (!Directory.Exists(target))
+            {
+                FailureReason = $"Directory '{target}' does not exist.";
+                return Task.FromResult(StageResult.Failed);
+            }
+
+            if (!Directory.Exists(destination))
+            {
+                FailureReason = $"Directory '{destination}' does not exist.";
+                return Task.FromResult(StageResult.Failed);
+            }
+
+            OnConsoleOut($"UBT: Moving folder '{target}' to '{destination}'");
+            Directory.Move(target, destination);
+            return Task.FromResult(StageResult.Successful);
+        }
+    }
+}

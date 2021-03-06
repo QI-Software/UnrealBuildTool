71ab3a572b239100afa83fedf873e0553f9b1b00
Erlite <ys.aameziane@gmail.com>
2021-03-06 02:48:06 +0100

    Add CheckFolderExists stage

---
+++ UnrealBuildTool.Build/Stages/CheckFolderExists.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace UnrealBuildTool.Build.Stages
+{
+    public class CheckFolderExists : BuildStage
+    {
+        public override string GetName() => nameof(CheckFolderExists);
+
+        public override string GetDescription()
+        {
+            TryGetConfigValue("FolderPath", out string path);
+            path = path.Replace(@"\", "/").Replace("//", "/").TrimEnd('/');
+            return $"Check folder '{path}'";
+        }
+
+        public override bool IsStageConfigurationValid(out string ErrorMessage)
+        {
+            if (!base.IsStageConfigurationValid(out ErrorMessage))
+            {
+                return false;
+            }
+
+            TryGetConfigValue("FolderPath", out string path);
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                ErrorMessage = $"Cannot check null or empty folder path.";
+                return false;
+            }
+
+            return true;
+        }
+
+        public override void GenerateDefaultStageConfiguration()
+        {
+            base.GenerateDefaultStageConfiguration();
+            
+            AddDefaultConfigurationKey("FolderPath", "");
+        }
+
+        public override Task<StageResult> DoTaskAsync()
+        {
+            TryGetConfigValue("FolderPath", out string path);
+            path = path.Replace(@"\", "/").Replace("//", "/").TrimEnd('/');
+
+            if (Directory.Exists(path))
+            {
+                return Task.FromResult(StageResult.Successful);
+            }
+
+            return Task.FromResult(StageResult.Failed);
+        }
+    }
+}
